be300a567f0899e8e147452d8855cc6538b01a87
Ismail-Nd <ndismail007@gmail.com>
2025-03-26 22:41:47 +0200

    added navbar and a hero section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Hero from './components/Hero'
+
+export default function App() {
+  return (
+    <div className=''>
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
